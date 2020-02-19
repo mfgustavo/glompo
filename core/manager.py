7af2a5a905145b27a0c4291bc554e118da51e395
mfgustavo <michael.freitasgustavo@ugent.be>
2020-02-19 17:13:30 +0100

    Implementation of a Gaussian Process

--- core/manager.py
+++ core/manager.py
@@ -5,8 +5,8 @@ from typing import *
 import multiprocessing as mp
 
 # Package imports
-from scope.scope import ParallelOptimizerScope
-from optimizers.baseoptimizer import BaseOptimizer
+from ..scope.scope import ParallelOptimizerScope
+from ..optimizers.baseoptimizer import BaseOptimizer
 
 
 class Result(NamedTuple):
@@ -22,23 +22,21 @@ class GloMPOOptimizer:
         intelligently restart others. """
 
     def __init__(self,
-                 task: Callable[Sequence[float], float],
-                 optimizers: Dict[str, Callable[Sequence[Any],]],
+                 task: Callable[[Sequence[float]], float],
+                 n_parms: int,
+                 optimizers: Dict[str, Callable],
                  bounds: Sequence[Tuple[float, float]],
-                 max_jobs: int,
-
-                 convergence_criteria: str,
-                 x0_criteria: str,
+                 max_jobs: Optional[int] = None,
+                 task_args: Optional[Tuple] = None,
+                 task_kwargs: Optional[Dict] = None,
+                 convergence_criteria: str = 'sing_conv',
+                 x0_criteria: str = 'rand',
                  tmax: Optional[float] = None,
                  omax: Optional[int] = None,
-
-
-
+                 fmax: Optional[int] = None,
                  region_stability_check: bool = False,
                  report_statistics: bool = False,
                  history_logging: int = 0,
-
-
                  visualisation: bool = False,
                  visualisation_args: Optional[Dict[str, Any]] = None):
         """
@@ -46,10 +44,12 @@ class GloMPOOptimizer:
 
         Parameters
         ----------
-        task: Callable[Sequence[float], float]
+        task : Callable[[Sequence[float]], float]
             Function to be minimized. Accepts a 1D sequence of parameter values and returns a single value.
             Note: Must be a standalone function which makes no modifications outside of itself.
-        optimizers: dict, Callables
+        n_parms : int
+            The number of parameters to be optimized.
+        optimizers : Dict[str, Callable]
             Dictionary of callable optimization functions (which are children of the BaseOptimizer class) with keywords
             describing their behaviour. Recognized keywords are:
                 'default': The default optimizer used if any of the below keywords are not set. This is the only
@@ -58,18 +58,15 @@ class GloMPOOptimizer:
                 'late': Strong local optimizers which are best suited to refining solutions in regions with known good
                     solutions.
                 'noisy': Optimizer which should be used in very noisy areas with very steep gradients or discontinuities
-        bounds: Sequence[Tuple[float, float]]
+        bounds : Sequence[Tuple[float, float]]
             Sequence of tuples of the form (min, max) limiting the range of each parameter.
-
-
         max_jobs : int
-            The maximum number of local optimizers run in parallel.
-        visualisation : bool
-            If True then a dynamic plot is generated to demonstrate the performance of the optimizers. Further options
-            (see visualisation_args) allow this plotting to be recorded and saved as a film.
-        visualisation_args : Union[dict, None]
-            Optional arguments to parameterize the dynamic plotting feature.
-
+            The maximum number of local optimizers run in parallel at one time. Defaults to one less than the number of
+            CPUs available to the system with a minimum of 1
+        task_args : Optional[Tuple]]
+            Optional arguments passed to task with every call.
+        task_kwargs : Optional[Dict]
+            Optional keyword arguments passed to task with every call.
         convergence_criteria: str
             Criteria used for convergence. Supported arguments:
                 'omax': The manager delivers the best answer obtained after initialising omax optimizers. Note that
@@ -80,9 +77,9 @@ class GloMPOOptimizer:
                 'n_conv':  The manager delivers the answer obtained by the nth optimizer to be allowed to reach full
                     convergence. Replace n with an integer value.
                     WARNING: It is strongly recommended that this criteria is used with some early termination criteria
-                        such as omax or tmax since later optimizations may not improve on earlier runs and thus may not
+                        such as omax, tmax or fmax since later optimizations may not improve on earlier runs and thus may not
                         be allowed to converge, hence this criteria is never reached.
-        x0_criteria: str
+        x0_criteria : str
             Criteria used for selection of the next starting point. Supported arguments:
                 'gp': A Guassian process regression is used to estimate areas with low error. Requires many function
                     evaluations (i.e many optimizers started) to begin producing effective suggestions.
@@ -91,21 +88,80 @@ class GloMPOOptimizer:
                 'es+gp': Uses 'es' for the early part of the optimization and switches to 'gp' in the late stage after
                     sufficient information has be acquired.
                 'rand': New starting points are selected entirely randomly.
-        tmax: Optional[float]
-        omax: Optional[int]
+        tmax : Optional[float]
+            Maximum number of seconds the optimizer is allowed to run before exiting (gracefully) and delivering the
+            best result seen up to this point.
+            Note: If tmax is reached GloMPO will not run region_stability_checks.
+        omax : Optional[int]
+            Maximum number of optimizers (in total) that can be started by the manager.
+        fmax : Optional[int]
+            Maximum number of function calls that are allowed between all optimizers.
         region_stability_check: bool = False
+            If True, local optimizers are started around a candidate solution which has been selected as a final
+            solution. This is used to measure its reproducibility. If the check fails, the manager resumes looking for
+            another solution.
         report_statistics: bool = False
+            If True, the manager reports the statistical significance of the suggested solution.
         history_logging: int = 0
+            Indicates the level of logging the user would like:
+                0 - No log files are saved.
+                1 - The log file of the optimizer from which the final solution was extracted is saved.
+                2 - The log file of every started optimizer is saved.
+                3 - The log file and screen output of every optimizer is saved.
+        visualisation : bool
+            If True then a dynamic plot is generated to demonstrate the performance of the optimizers. Further options
+            (see visualisation_args) allow this plotting to be recorded and saved as a film.
+        visualisation_args : Union[dict, None]
+            Optional arguments to parameterize the dynamic plotting feature. See ParallelOptimizationScope.
         """
+        def task_args_wrapper(func, *args, **kwargs):
+            def wrapper(x):
+                return func(x, *args, **kwargs)
+            return wrapper
+
+        if not callable(task):
+            raise TypeError(f"{task} is not callable.")
+        self.task = task_args_wrapper(task, task_args, task_kwargs)
+
+        if n_parms > 0 and isinstance(n_parms, int):
+            self.n_parms = n_parms
+        else:
+            raise ValueError(f"Cannot parse n_parms = {n_parms}. Only positive integers are allowed.")
 
         if 'default' not in optimizers:
             raise ValueError("'default' not found in optimizer dictionary. This value must be set.")
         for optimizer in optimizers.values():
             if not isinstance(optimizer, BaseOptimizer):
                 raise TypeError(f"{optimizer} not an instance of BaseOptimizer.")
-
         self.optimizers = optimizers
-        self.max_jobs = max_jobs
+
+        if len(bounds) != n_parms:
+            raise ValueError(f"Number of parameters (n_parms) and number of bounds are not equal")
+        for bnd in bounds:
+            if bnd[0] == bnd[1]:
+                raise ValueError(f"Bounds min and max cannot be equal. Rather fix its value and remove it from the "
+                                 f"list of parameters. Fixed values can be supplied through task_args or task_kwargs.")
+            if bnd[1] < bnd[0]:
+                raise ValueError(f"Bound min cannot be larger than max.")
+
+        if isinstance(max_jobs, int):
+            if max_jobs > 0:
+                self.max_jobs = max_jobs
+            else:
+                raise ValueError(f"Cannot parse max_jobs = {max_jobs}. Only positive integers are allowed.")
+        else:
+            raise TypeError(f"Cannot parse max_jobs = {max_jobs}. Only positive integers are allowed.")
+
+        if convergence_criteria in ['omax', 'sing_conv', 'n_conv']:
+            self.convergence_criteria = convergence_criteria
+        else:
+            raise ValueError(f"Cannot parse convergence_criteria = {convergence_criteria}. See docstring for allowed "
+                             f"values.")
+
+        if x0_criteria in ['gp', 'es', 'es+gp', 'rand']:
+            self.x0_criteria = x0_criteria
+        else:
+            raise ValueError(f"Cannot parse x0_criteria = {x0_criteria}. See docstring for allowed values.")
 
         self.optimizer_jobs = []
         self.hyperparm_jobs = []
--- tests/test_manager.py
+++ tests/test_manager.py
@@ -1,2 +1,174 @@
+from typing import Callable, Sequence, Tuple
+
 import pytest
 
+from ..core.manager import GloMPOOptimizer
+from ..optimizers.baseoptimizer import BaseOptimizer
+
+
+class TestMangerInit:
+
+    class OptimizerTest1(BaseOptimizer):
+        needscaler = False
+        signal_pipe = None
+        results_queue = None
+
+        def minimize(self, function: Callable, x0: Sequence[float], bounds: Sequence[Tuple[float, float]],
+                     callbacks: Callable = None, **kwargs):
+            pass
+
+    class OptimizerTest2:
+        pass
+
+    def test_task_raises(self):
+        with pytest.raises(TypeError):
+            GloMPOOptimizer(None,
+                            2,
+                            {'default': self.OptimizerTest1()},
+                            ((0, 1), (0, 1)),
+                            5)
+
+    def test_task_notraises(self):
+        GloMPOOptimizer(lambda x, y: x+y,
+                        2,
+                        {'default': self.OptimizerTest1()},
+                        ((0, 1), (0, 1)),
+                        5)
+
+    def test_optimizer_raisesnotbase(self):
+        with pytest.raises(TypeError):
+            GloMPOOptimizer(lambda x, y: x+y,
+                            2,
+                            {'default': self.OptimizerTest2()},
+                            ((0, 1), (0, 1)),
+                            5)
+
+    def test_optimizer_raisesnotdefault(self):
+        with pytest.raises(ValueError):
+            GloMPOOptimizer(lambda x, y: x+y,
+                            2,
+                            {'other': self.OptimizerTest1()},
+                            ((0, 1), (0, 1)),
+                            5)
+
+    def test_maxjobs_0(self):
+        with pytest.raises(ValueError):
+            GloMPOOptimizer(lambda x, y: x+y,
+                            2,
+                            {'default': self.OptimizerTest1()},
+                            ((0, 1), (0, 1)),
+                            0)
+
+    def test_maxjobs_neg(self):
+        with pytest.raises(ValueError):
+            GloMPOOptimizer(lambda x, y: x+y,
+                            2,
+                            {'default': self.OptimizerTest1()},
+                            ((0, 1), (0, 1)),
+                            -5)
+
+    def test_maxjobs_float(self):
+        with pytest.raises(TypeError):
+            GloMPOOptimizer(lambda x, y: x+y,
+                            2,
+                            {'default': self.OptimizerTest1()},
+                            ((0, 1), (0, 1)),
+                            2.5646)
+
+    def test_maxjobs_nan(self):
+        with pytest.raises(TypeError):
+            GloMPOOptimizer(lambda x, y: x+y,
+                            2,
+                            {'default': self.OptimizerTest1()},
+                            ((0, 1), (0, 1)),
+                            None)
+
+    def test_bounds_diffdims(self):
+        with pytest.raises(ValueError):
+            GloMPOOptimizer(lambda x: x+1,
+                            1,
+                            {'default': self.OptimizerTest1()},
+                            ((0, 1), (0, 1)),
+                            5)
+
+    def test_bounds_equalminmax1(self):
+        with pytest.raises(ValueError):
+            GloMPOOptimizer(lambda x, y: x+y,
+                            2,
+                            {'default': self.OptimizerTest1()},
+                            ((1, 1), (0, 1)),
+                            5)
+
+    def test_bounds_equalminmax2(self):
+        with pytest.raises(ValueError):
+            GloMPOOptimizer(lambda x, y: x+y,
+                            2,
+                            {'default': self.OptimizerTest1()},
+                            ((0, 1), (1, 1)),
+                            5)
+
+    def test_bounds_equalminmax3(self):
+        with pytest.raises(ValueError):
+            GloMPOOptimizer(lambda x, y: x+y,
+                            2,
+                            {'default': self.OptimizerTest1()},
+                            ((1, 1), (1, 1)),
+                            5)
+
+    def test_bounds_wrongminmax1(self):
+        with pytest.raises(ValueError):
+            GloMPOOptimizer(lambda x, y: x+y,
+                            2,
+                            {'default': self.OptimizerTest1()},
+                            ((2, 1), (0, 1)),
+                            5)
+
+    def test_bounds_wrongminmax2(self):
+        with pytest.raises(ValueError):
+            GloMPOOptimizer(lambda x, y: x+y,
+                            2,
+                            {'default': self.OptimizerTest1()},
+                            ((0, 1), (2, 1)),
+                            5)
+
+    def test_bounds_wrongminmax3(self):
+        with pytest.raises(ValueError):
+            GloMPOOptimizer(lambda x, y: x+y,
+                            2,
+                            {'default': self.OptimizerTest1()},
+                            ((2, 1), (2, 1)),
+                            5)
+
+    def test_x0crit1(self):
+        GloMPOOptimizer(lambda x, y: x+y,
+                        2,
+                        {'default': self.OptimizerTest1()},
+                        ((0, 1), (0, 1)),
+                        5,
+                        x0_criteria='rand')
+
+    def test_x0crit2(self):
+        with pytest.raises(ValueError):
+            GloMPOOptimizer(lambda x, y: x+y,
+                            2,
+                            {'default': self.OptimizerTest1()},
+                            ((0, 1), (0, 1)),
+                            5,
+                            x0_criteria='not_allowed')
+
+    def test_convcrit1(self):
+        GloMPOOptimizer(lambda x, y: x+y,
+                        2,
+                        {'default': self.OptimizerTest1()},
+                        ((0, 1), (0, 1)),
+                        5,
+                        convergence_criteria='sing_conv')
+
+    def test_convcrit2(self):
+        with pytest.raises(ValueError):
+            GloMPOOptimizer(lambda x, y: x+y,
+                            2,
+                            {'default': self.OptimizerTest1()},
+                            ((0, 1), (0, 1)),
+                            5,
+                            convergence_criteria='not_allowed')
