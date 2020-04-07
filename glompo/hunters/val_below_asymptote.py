b7110ec1523b69cb9a2c362cab0ff36bb13cceee
mfgustavo <michael.freitasgustavo@ugent.be>
2020-04-07 17:23:30 +0200

    Bugfixes Adaptation of scope to new hunting scheme Applied evaluation laziness to hunters and checkers

--- glompo/hunters/val_below_asymptote.py
+++ glompo/hunters/val_below_asymptote.py
@@ -1,13 +1,10 @@
 
 
-from typing import *
-
 import numpy as np
 
 from .basehunter import BaseHunter
 from ..core.logger import Logger
 from ..core.regression import DataRegressor
-from ..core.scope import GloMPOScope
 
 
 __all__ = ("ValBelowAsymptote",)
@@ -15,18 +12,11 @@ __all__ = ("ValBelowAsymptote",)
 
 class ValBelowAsymptote(BaseHunter):
 
-    def __init__(self, scope: Optional[GloMPOScope] = None):
+    def __init__(self):
         """ Returns True if the current best value seen by the hunter falls below the 95% confidence threshold of the
             victim.
-
-            Parameters
-            ----------
-            scope: Optional[GloMPOScope] = None
-                Accepts a scope object from the manager. If given the results from the regressions will be used to
-                update the asymptotes and uncertainties for each optimizer.
         """
         self.regressor = DataRegressor()
-        self.scope = scope
 
     def is_kill_condition_met(self, log: Logger, hunter_opt_id: int, victim_opt_id: int) -> bool:
         hunter_vals = log.get_history(hunter_opt_id, "fx_best")
@@ -34,12 +24,14 @@ class ValBelowAsymptote(BaseHunter):
         victim_t = np.array(range(len(victim_y)))
 
         if len(hunter_vals) > 0:
-            med, low, upp = victim_y[-1] * self.regressor.estimate_parameters(victim_t, victim_y,
-                                                                              parms='asymptote',
-                                                                              cache_key=victim_opt_id)
-            if self.scope:
-                self.scope.update_mean(victim_opt_id, med, low, upp)
+            result = self.regressor.estimate_parameters(victim_t, victim_y,
+                                                        parms='asymptote',
+                                                        nsteps=3000,
+                                                        nwalkers=25,
+                                                        cache_key=victim_opt_id)
 
-            return hunter_vals[-1] < low
+            if len(result) == 3:
+                med, low, upp = tuple(victim_y[-1] * val for val in result)
+                return hunter_vals[-1] < low
 
         return False
