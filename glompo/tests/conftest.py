8e01f1392d81b7f6fac464f55a7cb46577669082
mfgustavo <michael.freitasgustavo@ugent.be>
2020-04-30 15:22:09 +0200

    Split scope tests from core tests to avoid matplotlib compatibility issues

--- glompo/tests/conftest.py
+++ glompo/tests/conftest.py
@@ -9,13 +9,8 @@ def pytest_addoption(parser):
                                                               "inspection.")
     parser.addoption("--run-minimize", "-M", action="store_true", help="runs a minimum working example of GloMPO "
                                                                        "minimisation")
-    parser.addoption("--run-scope-tests", "-S", action="store_true", help="runs test_scope.py. Off by default to "
-                                                                          "avoid compatibility issues with matplotlib "
-                                                                          "in some environments.")
 
 
 def pytest_runtest_setup(item):
     if 'mini' in item.keywords and not item.config.getvalue("--run-minimize"):
         pytest.skip("need --run-minimize or -M option to run")
-    if 'scope' in item.keywords and not item.config.getvalue('--run-scope-tests'):
-        pytest.skip("need --run-scope-tests or -S option to run")
---
+++ glompo/tests/scope_tests/__init__.py
@@ -0,0 +1,2 @@
+
+""" Contains tests for the GloMPO scope which is an optional feature that can cause issues in some environments. """
