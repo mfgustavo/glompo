b7334315af582802a69abd4f17667d601239565e
mfgustavo <michael.freitasgustavo@ugent.be>
2020-05-12 17:00:42 +0200

    New hunter to terminate optimizers stuck in a region of non-finite results

---
+++ glompo/hunters/lastptsinvalid.py
@@ -0,0 +1,31 @@
+
+
+import numpy as np
+
+from .basehunter import BaseHunter
+from ..core.optimizerlogger import OptimizerLogger
+from ..core.regression import DataRegressor
+
+
+__all__ = ("LastPointsInvalid",)
+
+
+class LastPointsInvalid(BaseHunter):
+
+    def __init__(self, n_iters: int = 1):
+        """ Some pathological functions may have undefined regions within them or combinations of parameters which
+            return non-finite results. This hunter can be used to terminate an optimizer which has not found a valid
+            iteration within its last n_iters number of iterations.
+        """
+        super().__init__()
+        self.n_iters = n_iters
+
+    def __call__(self,
+                 log: OptimizerLogger,
+                 regressor: DataRegressor,
+                 hunter_opt_id: int,
+                 victim_opt_id: int) -> bool:
+
+        fcalls = log.get_history(victim_opt_id, "fx")[-self.n_iters:]
+        self._last_result = len(fcalls) >= self.n_iters and np.isfinite(fcalls).all()
+        return self._last_result
