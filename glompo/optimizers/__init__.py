92601aa197c0f902fd9bf14eed86f7ad50f87a37
mfgustavo <michael.freitasgustavo@ugent.be>
2020-06-22 10:48:01 +0200

    Remove importing to reduce compatibility issues

--- glompo/optimizers/__init__.py
+++ glompo/optimizers/__init__.py
@@ -2,14 +2,3 @@
 
 """ Subpackage containing BaseOptimizer and several GloMPO compatible optimization algorithm implementation. """
 
-
-from .baseoptimizer import BaseOptimizer
-from .cmawrapper import CMAOptimizer
-from .gflswrapper import GFLSOptimizer
-from .nevergrad import Nevergrad
-
-
-__all__ = ("BaseOptimizer",
-           "CMAOptimizer",
-           "GFLSOptimizer",
-           "Nevergrad")
