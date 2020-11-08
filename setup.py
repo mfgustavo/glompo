236fe86172ee8bfeeb3823a68410e71213bc0832
mfgustavo <michael.freitasgustavo@ugent.be>
2020-11-08 17:56:05 +0100

    Bugfix: CMA incorrectly pushing best ever result not best iteration result

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def get_readme():
 
 setup(
     name="glompo",
-    version="2.0.1",
+    version="2.0.2",
     description="Globally managed parallel optimization",
     long_description=get_readme(),
     author="Michael Freitas Gustavo",
