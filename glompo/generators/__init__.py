ef5f0e3281d183e86d8ae330f4e8aceee57f90bd
mfgustavo <michael.freitasgustavo@ugent.be>
2021-07-19 14:27:14 +0200

    Bugfix: Remove generator with optional dependency

--- glompo/generators/__init__.py
+++ glompo/generators/__init__.py
@@ -1,12 +1,10 @@
 from .basegenerator import BaseGenerator
 from .best import IncumbentGenerator
 from .exploit_explore import ExploitExploreGenerator
-from .peterbation import PerturbationGenerator
 from .random import RandomGenerator
 from .single import SinglePointGenerator
 
 __all__ = ("BaseGenerator",
-           "PerturbationGenerator",
            "RandomGenerator",
            "IncumbentGenerator",
            "SinglePointGenerator",
