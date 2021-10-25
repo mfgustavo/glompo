56d4fe1e55aa71bd10ba3ba789fdbb18aa68afd5
mfgustavo <michael.freitasgustavo@ugent.be>
2021-10-25 10:35:51 +0200

    Fix version number parsing.

--- glompo/_version.py
+++ glompo/_version.py
@@ -1,2 +1,2 @@
-__version__ = '3.1.1.post_dev'
-__version_info__ = tuple(map(int, __version__.split('.')))
+__version_info__ = (3, 1, 1, 'post_dev')
+__version__ = '.'.join(map(str, __version_info__))
