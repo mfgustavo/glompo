37161301ee0dac11e4c70b94581c0da8893d4ae7
mfgustavo <michael.freitasgustavo@ugent.be>
2021-11-02 09:17:35 +0100

    Add important todo for when classes are finalized.

--- glompo/interfaces/params/__init__.py
+++ glompo/interfaces/params/__init__.py
@@ -10,3 +10,5 @@ __all__ = ("Optimization",
            "setup_reax_from_classic",
            "setup_reax_from_params",
            "setup_xtb_from_params",)
+
+# todo make sure params interface docs build correctly.
