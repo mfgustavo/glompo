c1df5e228d40d1de722aad93d19224b8afa10654
mfgustavo <michael.freitasgustavo@ugent.be>
2022-03-11 16:29:46 +0100

    Allow parallelisation of the RandomOptimizer

--- glompo/optimizers/random.py
+++ glompo/optimizers/random.py
@@ -1,6 +1,7 @@
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 from multiprocessing import Event, Queue
 from multiprocessing.connection import Connection
-from typing import Callable, Sequence, Tuple
+from typing import Callable, Optional, Sequence, Tuple
 
 import numpy as np
 
@@ -29,57 +30,61 @@ class RandomOptimizer(BaseOptimizer):
                  _is_log_detailed: bool = False,
                  workers: int = 1,
                  backend: str = 'processes',
-                 iters: int = 100):
+                 iters: int = 100,
+                 seed: Optional[int] = None):
         super().__init__(_opt_id, _signal_pipe, _results_queue, _pause_flag, _is_log_detailed, workers, backend)
         self.max_iters = iters
         self.used_iters = 0
         self.result = MinimizeResult()
         self.stop_called = False
+        self.seed = seed
         self.logger.debug("Setup optimizer")
 
     def minimize(self,
                  function: Callable,
                  x0: Sequence[float],
                  bounds: Sequence[Tuple[float, float]],
-                 callbacks: Callable = None, **kwargs) -> MinimizeResult:
+                 **kwargs) -> MinimizeResult:
+        if self.seed:
+            np.random.seed(self.seed)
 
-        best_f = self.result.fx if self.is_restart else np.inf
         bounds = np.transpose(bounds)
 
-        while self.used_iters < self.max_iters:
+        def evaluate(_):
+            x = np.random.uniform(bounds[0], bounds[1])
+            fx = function(x)
+            return x, fx
+
+        if self.workers > 1:
+            if self._backend == 'threads':
+                executor = ThreadPoolExecutor(self.workers)
+            else:
+                executor = ProcessPoolExecutor(self.workers)
+            generator = executor.map(evaluate, range(self.used_iters, self.max_iters))
+        else:
+            generator = map(evaluate, range(self.used_iters, self.max_iters))
+
+        for x, fx in generator:
+            self.used_iters += 1  # Used for checkpointing and restarts
+            if fx < self.result.fx:
+                self.result.fx = fx
+                self.result.x = x
+            if self._results_queue:
+                self.check_messages()
+                self._pause_signal.wait()
             if self.stop_called:
                 break
 
-            self.used_iters += 1
-            vector = np.random.uniform(bounds[0], bounds[1])
-            self.logger.debug("Generated vector = %s", vector)
+        self.result.success = not bool(self.stop_called)
 
-            self.logger.debug("Evaluating function.")
-            fx = function(vector)
-            self.logger.debug("Function returned fx = %f", fx)
+        if self.workers > 1:
+            executor.shutdown()
 
-            if callbacks and callbacks():
-                self.stop_called = True
-
-            if self._results_queue:
-                self.logger.debug("Checking messages")
-                self.check_messages()
-                self._pause_signal.wait()
-
-            if fx < best_f:
-                best_f = fx
-                best_x = vector
-                self.logger.debug("Updating best")
-                self.result.x, self.result.fx = best_x, best_f
-
-        self.result.success = True
-        self.logger.debug("Termination successful")
         if self._results_queue:
             self.logger.debug("Messaging manager")
             self.message_manager(0, "Optimizer convergence")
             self.check_messages()
 
-        self.logger.debug("Returning result")
         return self.result
 
     def callstop(self, reason: str = ""):
