a0f3570a327279c996cee24f40831bf40336c436
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-17 14:56:58 +0000

    Adding tarfile member sanitization to extractall()

--- glompo/core/manager.py
+++ glompo/core/manager.py
@@ -705,7 +705,29 @@ class GloMPOManager:
         tmp_dir = Path(tmp_dir_obj.name)
 
         with tarfile.open(path, 'r:gz') as tfile:
-            tfile.extractall(tmp_dir)
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tfile, tmp_dir)
 
         # Load manager variables
         try:
